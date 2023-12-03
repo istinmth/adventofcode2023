0c49a7e83cb30a79af1f8dfae8f9c4131be78645
istinmth <istinmth@icloud.com>
2023-12-03 21:07:56 +0100

    day 03

--- day02/obj/Debug/net8.0/day2.AssemblyInfo.cs
+++ day02/obj/Debug/net8.0/day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f146671ad2e6f7f82cc35c0956167736aedb4b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
