84a78edcb558fe9e24d50119c88b14167f29a296
istinmth <istinmth@icloud.com>
2023-12-11 08:58:09 +0100

    day11

---
+++ day11/galaxy.m
@@ -0,0 +1,13 @@
+day = 11;
+inputPath = "galaxy.txt";
+input = Characters /@ Import[inputPath, "List"];
+
+expandUniverse[factor_, array_] :=
+ Module[{oldPos, newPos, subs},
+    oldPos = Position[array, "#"];
+    newPos = oldPos + (factor - 1) * Count[Position[array, " "], _?(# < oldPos &)];
+    subs = Subsets[newPos, {2}];
+    Total[ManhattanDistance @@ # & /@ subs]]
+
+expandUniverse[2, input]
+expandUniverse[10^6, input]
